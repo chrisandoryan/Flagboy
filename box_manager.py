efc7c2a79917871e714413f566b5a5171a485ad2
Chrisando Ryan <chrisando.siahaan@gmail.com>
2023-08-23 12:52:55 +0700

    add service aliveness scanner function

--- box_manager.py
+++ box_manager.py
@@ -7,12 +7,33 @@ load_dotenv()
 BASE_URL = os.environ["CTFD_URL"]
 TOKEN = os.environ["CTFD_TOKEN"]
 
+def check_services(target, port_list):
+    success_count = 0
+    pl = port_list.copy()
+
+    ssh_port = 22
+    if "ssh_port" in target:
+        ssh_port = target['ssh_port']
+        pl.append(ssh_port)
+
+    for p in pl:
+        print(f"Checking {target['ip_address']}:{p}")
+        result = get_heartbeat(target['ip_address'], p)
+        if (result):
+            print("[+] Alive")
+            success_count += 1
+        else:
+            print("[-] Dead")
+            
+    return success_count >= 3
+
 def get_heartbeat(target_host, target_port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(5)
     result = sock.connect_ex((target_host, target_port))
     return result == 0
 
-def inject_flag(target_host, flag, mode="ROOT"):
+def inject_flag(target, flag, mode="ROOT"):
     if mode == "ROOT":
         username = "root"
         password = os.environ["BOX_ROOT_PASSWORD"]
@@ -25,15 +46,29 @@ def inject_flag(target_host, flag, mode="ROOT"):
     ssh_username = os.environ["BOX_SA_NAME"]
     ssh_password = os.environ["BOX_SA_PASSWORD"]
 
-    s = paramiko.SSHClient()
-    s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    s.load_system_host_keys()
-    s.connect(target_host, 22, ssh_username, ssh_password)
-    command = "echo '%s' > %s; chown %s:%s %s" % (flag, flag_path, username, username, flag_path)
-    print(command)
+    ssh_port = 22
+    if "ssh_port" in target:
+        ssh_port = target['ssh_port']
+        print(f"[!] Switching SSH port to {ssh_port}")
 
-    stdin, stdout, stderr = s.exec_command(command)
-    for line in stdout.readlines():
-        print(line)
-    s.close()
-    return stdout.channel.recv_exit_status() == -1
+    try:
+        s = paramiko.SSHClient()
+        s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        s.load_system_host_keys()
+        s.connect(target['ip_address'], ssh_port, ssh_username, ssh_password)
+        command = "echo '%s' | sudo -S /bin/sh -c \"echo '%s' > %s\"" % (ssh_password, flag, flag_path)
+        print(f"\t[o] {command}")
+
+        stdin, stdout, stderr = s.exec_command(command)
+        for line in stdout.readlines():
+            print(f"[!!!] {line}")
+        s.close()
+
+        recv_status = stdout.channel.recv_exit_status()
+        print(f"[!!!] Recv Status: {recv_status}")
+        return recv_status >= 0
+    except Exception as e:
+        print(f"[!!!] SSH Error!")
+        print(e)
+        
+        return False
--- main.py
+++ main.py
@@ -6,25 +6,33 @@ import json
 f = open('hosts.json')
 targets = json.load(f)
 
+known_ports = [80, 22, 64573]
+check_only = False
+
 for t in targets:
+    print()
     print(f"[+] Configuring {t['ip_address']} for {t['name']}")
-
-    user_flag = flagman.generate_flag()
-    inject_success = boxman.inject_flag(t['ip_address'], user_flag, mode="USER")
-    print(f"[+] User flag is injected with value {user_flag}. Status: {inject_success}")
-    if (inject_success):
-        push_user_chall = ctfdman.create_challenge(name=f"[USER] {t['ip_address']}", description=t['ip_address'], category=t['name'])
-        print(f"[+] Pushed user flag to CTFd with value {user_flag}. Status: {push_user_chall['success']}") 
-        push_user_flag = ctfdman.create_flag(challenge_id=push_user_chall['data']['id'], flag=user_flag)
+    should_inject = boxman.check_services(t, known_ports)
     
-    root_flag = flagman.generate_flag()
-    inject_success = boxman.inject_flag(t['ip_address'], root_flag, mode="ROOT")   
-    print(f"[+] Root flag is injected with value {root_flag}. Status: {inject_success}") 
-    if (inject_success):
-        push_root_chall = ctfdman.create_challenge(name=f"[ROOT] {t['ip_address']}", description=t['ip_address'], category=t['name'])
-        print(f"[+] Pushed root flag to CTFd with value {root_flag}. Status: {push_root_chall['success']}") 
-        push_root_flag = ctfdman.create_flag(challenge_id=push_root_chall['data']['id'], flag=root_flag)    
-
-
+    if should_inject and not check_only:
+        user_flag = flagman.generate_flag()
+        inject_success = boxman.inject_flag(t, user_flag, mode="USER")
+        print(f"[+] User flag is injected with value {user_flag}. Status: {inject_success}")
+        if (inject_success):
+            push_user_chall = ctfdman.create_challenge(name=f"[USER] {t['ip_address']}", description=t['ip_address'], category=t['name'])
+            print(f"[+] Pushed user flag to CTFd with value {user_flag}. Status: {push_user_chall['success']}") 
+            push_user_flag = ctfdman.create_flag(challenge_id=push_user_chall['data']['id'], flag=user_flag)
+        
+        root_flag = flagman.generate_flag()
+        inject_success = boxman.inject_flag(t, root_flag, mode="ROOT")   
+        print(f"[+] Root flag is injected with value {root_flag}. Status: {inject_success}") 
+        if (inject_success):
+            push_root_chall = ctfdman.create_challenge(name=f"[ROOT] {t['ip_address']}", description=t['ip_address'], category=t['name'])
+            print(f"[+] Pushed root flag to CTFd with value {root_flag}. Status: {push_root_chall['success']}") 
+            push_root_flag = ctfdman.create_flag(challenge_id=push_root_chall['data']['id'], flag=root_flag)    
+    else:
+        print(f"[!] Services of {t['name']} are unstable. Skipping...")
+    
+    print()
 
 
